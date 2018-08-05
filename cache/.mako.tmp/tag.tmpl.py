1a599de6ac73782bdeb39e8993af76895f86671d
dogen <3393973+necromuralist@users.noreply.github.com>
2018-08-04 19:32:15 -0700

    Nikola auto commit.
    Nikola version: 7.8.15

---
+++ cache/.mako.tmp/tag.tmpl.py
@@ -0,0 +1,151 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1533436335.2024124
+_enable_loop = True
+_template_filename = '/home/dogen/.virtualenvs/kaggle-competitions/lib/python3.6/site-packages/nikola/data/themes/base/templates/tag.tmpl'
+_template_uri = 'tag.tmpl'
+_source_encoding = 'utf-8'
+_exports = ['extra_head', 'content']
+
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    ns = runtime.TemplateNamespace('feeds_translations', context._clean_inheritance_tokens(), templateuri='feeds_translations_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'feeds_translations')] = ns
+
+def _mako_inherit(template, context):
+    _mako_generate_namespaces(context)
+    return runtime._inherit_from(context, 'list_post.tmpl', _template_uri)
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        _import_ns = {}
+        _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
+        parent = _import_ns.get('parent', context.get('parent', UNDEFINED))
+        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
+        date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        description = _import_ns.get('description', context.get('description', UNDEFINED))
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
+        def content():
+            return render_content(context._locals(__M_locals))
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
+        __M_writer = context.writer()
+        __M_writer('\n')
+        __M_writer('\n\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'extra_head'):
+            context['self'].extra_head(**pageargs)
+        
+
+        __M_writer('\n\n\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_extra_head(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        parent = _import_ns.get('parent', context.get('parent', UNDEFINED))
+        def extra_head():
+            return render_extra_head(context)
+        __M_writer = context.writer()
+        __M_writer('\n    ')
+        __M_writer(str(parent.extra_head()))
+        __M_writer('\n    ')
+        __M_writer(str(feeds_translations.head(tag)))
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
+        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
+        date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        description = _import_ns.get('description', context.get('description', UNDEFINED))
+        posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
+        def content():
+            return render_content(context)
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        __M_writer = context.writer()
+        __M_writer('\n<article class="tagpage">\n    <header>\n        <h1>')
+        __M_writer(filters.html_escape(str(title)))
+        __M_writer('</h1>\n')
+        if description:
+            __M_writer('            <p>')
+            __M_writer(str(description))
+            __M_writer('</p>\n')
+        if subcategories:
+            __M_writer('        ')
+            __M_writer(str(messages('Subcategories:')))
+            __M_writer('\n        <ul>\n')
+            for name, link in subcategories:
+                __M_writer('            <li><a href="')
+                __M_writer(str(link))
+                __M_writer('">')
+                __M_writer(filters.html_escape(str(name)))
+                __M_writer('</a></li>\n')
+            __M_writer('        </ul>\n')
+        __M_writer('        <div class="metadata">\n            ')
+        __M_writer(str(feeds_translations.feed_link(tag)))
+        __M_writer('\n        </div>\n        ')
+        __M_writer(str(feeds_translations.translation_link()))
+        __M_writer('\n    </header>\n')
+        if posts:
+            __M_writer('        <ul class="postlist">\n')
+            for post in posts:
+                __M_writer('            <li><time class="listdate" datetime="')
+                __M_writer(str(post.formatted_date('webiso')))
+                __M_writer('" title="')
+                __M_writer(filters.html_escape(str(post.formatted_date(date_format))))
+                __M_writer('">')
+                __M_writer(filters.html_escape(str(post.formatted_date(date_format))))
+                __M_writer('</time> <a href="')
+                __M_writer(str(post.permalink()))
+                __M_writer('" class="listtitle">')
+                __M_writer(filters.html_escape(str(post.title())))
+                __M_writer('<a></li>\n')
+            __M_writer('        </ul>\n')
+        __M_writer('</article>\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "/home/dogen/.virtualenvs/kaggle-competitions/lib/python3.6/site-packages/nikola/data/themes/base/templates/tag.tmpl", "uri": "tag.tmpl", "source_encoding": "utf-8", "line_map": {"23": 3, "29": 0, "49": 2, "50": 3, "55": 8, "60": 39, "66": 5, "77": 5, "78": 6, "79": 6, "80": 7, "81": 7, "87": 11, "103": 11, "104": 14, "105": 14, "106": 15, "107": 16, "108": 16, "109": 16, "110": 18, "111": 19, "112": 19, "113": 19, "114": 21, "115": 22, "116": 22, "117": 22, "118": 22, "119": 22, "120": 24, "121": 26, "122": 27, "123": 27, "124": 29, "125": 29, "126": 31, "127": 32, "128": 33, "129": 34, "130": 34, "131": 34, "132": 34, "133": 34, "134": 34, "135": 34, "136": 34, "137": 34, "138": 34, "139": 34, "140": 36, "141": 38, "147": 141}}
+__M_END_METADATA
+"""
